5563b0b0d77488cb704a306487ab3148dcbc11ba
murilossx <murilo.santos1209@gmail.com>
2026-01-20 14:16:00 -0300

    Primeiro commit: Estrutura do projeto

---
+++ src/test/java/com/murilossx/myworkout/MyworkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilossx.myworkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyworkoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
